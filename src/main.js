ef5832185ecc8f25dfb0a58415b70d6c10356fdb
nav2612 <navpreet51198@gmail.com>
2023-03-01 19:36:14 -0800

    #1

--- src/main.js
+++ src/main.js
@@ -6,6 +6,14 @@ const Mainpage = () => {
 
 	return (
     <React.Fragment> 
+    <div style={{position: 'relative'}}>
+    <img src={'https://images.unsplash.com/photo-1506290690282-661fbd742be8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'} 
+    alt={'Blog Image'} style={{padding: '10px 15rem', height:'350px', width: '1420px'}}/>
+      <div style={{position: 'absolute', top: '70%', left: '50%', transform: 'translate(-50%, -50%)', color:'white'}}>
+        <h1>Grow with Open House</h1>
+        <p style={{textAlign:'center'}}>We provide you the best.</p>
+      </div>
+    </div>
     <Cards/>
     </React.Fragment>
 	);
--- src/navbar.js
+++ src/navbar.js
@@ -7,6 +7,13 @@ const navbar = () => {
         <div className='navbar'>
             <div className='container'>
                 <h1><span><BsFillHouseFill />Open</span>House</h1>
+                <ul className='nav-menu' style={{listStyleType:'none', justifyContent:'flex-end'}}>
+                    <li><a>Home</a></li>
+                    <li><a>Blog</a></li>
+                    <li><a>Contact Us</a></li>
+                    <li><a style={{backgroundColor: '#24a0ed', color: 'white', padding: '10px 20px', borderRadius: '25px'}}>
+                        Login</a></li>
+                </ul>
             </div>
         </div>
     );
