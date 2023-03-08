1910a5f4e4f5154d90746378291f4cb4e125d40d
nav2612 <navpreet51198@gmail.com>
2023-03-08 14:36:59 -0800

    #1

--- src/CardItem.js
+++ src/CardItem.js
@@ -1,33 +1,18 @@
-import { getDownloadURL, ref } from 'firebase/storage';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Cards.css';
-import { storage } from './firebase-config'; // import the Firebase storage module
 
 function CardItem(props) {
-  const [imageUrl, setImageUrl] = React.useState(''); // state to store the image URL
-
-  // function to get the download URL of the image from Firebase storage
-  const getImageUrl = async () => {
-    const imageRef = ref(storage,props.src);
-    const url = await getDownloadURL(imageRef);
-    setImageUrl(url);
-  };
-
-  React.useEffect(() => {
-    getImageUrl(); // get the image URL when the component mounts
-  }, [props.src]);
-
   return (
     <>
       <li className='cards__item'>
         <Link className='cards__item__link' to={props.path}>
-          <figure className='cards__item__pic-wrap' data-category={props.label}>
-            {/* use the imageUrl state variable as the src attribute of the image */}
-            <img className='cards__item__img' alt='Open House' src={imageUrl} />
+          <figure className='cards__item__pic-wrap'>
+            <img className='cards__item__img' alt='Open House' src={props.image} />
           </figure>
           <div className='cards__item__info'>
-            <h5 className='cards__item__text'>{props.text}</h5>
+            <h5 className="cards__item__heading">{props.heading}</h5>
+            <h6 className='cards__item__text'>{props.text}</h6>
           </div>
         </Link>
       </li>
--- src/Cards.js
+++ src/Cards.js
@@ -6,7 +6,23 @@ import "./Cards.css";
 
 function Cards() {
   const [cards, setCards] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
   const cardsRef = collection(db, "cards");
+  
+  const images = [
+    'https://images.unsplash.com/photo-1430285561322-7808604715df?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTZ8fHJlYWwlMjBlc3RhdGUlMjBob3VzZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
+    'https://images.unsplash.com/photo-1568092775154-7fa176a29c0f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+    'https://images.unsplash.com/photo-1560518883-ce09059eeffa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fHJlYWwlMjBlc3RhdGUlMjBob3VzZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
+    'https://images.unsplash.com/photo-1570129477492-45c003edd2be?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    'https://images.unsplash.com/photo-1605146769289-440113cc3d00?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    'https://images.unsplash.com/photo-1592595896616-c37162298647?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    'https://images.unsplash.com/photo-1449844908441-8829872d2607?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    'https://images.unsplash.com/photo-1628624747186-a941c476b7ef?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    'https://images.unsplash.com/photo-1597203294638-a359e99f4262?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    'https://images.unsplash.com/photo-1510627489930-0c1b0bfb6785?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    'https://images.unsplash.com/photo-1490197415175-074fd86b1fcc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1174&q=80',
+    'https://images.unsplash.com/photo-1480074568708-e7b720bb3f09?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1174&q=80'
+  ];
 
   useEffect(() => {
     async function fetchCards() {
@@ -17,21 +33,42 @@ function Cards() {
     fetchCards();
   }, []);
 
+  const totalPages = Math.ceil(cards.length / 9);
+  const displayCards = () => {
+    const startIndex = (currentPage - 1) * 9;
+    const endIndex = startIndex + 9;
+    return cards.slice(startIndex, endIndex).map((card) => (
+      <CardItem
+        key={card.id}
+        heading={card.heading}
+        text={card.description}
+        image={images[Math.floor(Math.random() * images.length)]}
+        path='blog_post'
+      />
+    ));
+  };
+
   return (
     <div className="cards">
       <div className="cards__container">
         <div className="cards__wrapper">
           <ul className="cards__items">
-            {cards.map((card) => (
-              <CardItem
-                key={card.id}
-                src={card.image}
-                text={card.description}
-                label={card.label}
-                path={card.path}
-              />
-            ))}
+            {displayCards()}
           </ul>
+        </div>
+        <div className="cards__pagination">
+          <button
+            onClick={() => setCurrentPage(currentPage - 1)}
+            disabled={currentPage === 1}
+          >
+            Previous
+          </button>
+          <button
+            onClick={() => setCurrentPage(currentPage + 1)}
+            disabled={currentPage === totalPages}
+          >
+            Next
+          </button>
         </div>
       </div>
     </div>
